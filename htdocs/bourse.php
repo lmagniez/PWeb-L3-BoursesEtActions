bcf8e83496f06afce217f80b90ab0edf80414816
Valembois Vincent <vincent_valembois@ens.univ-artois.fr>
2017-03-20 16:48:06 +0100

    bourse categorie

--- htdocs/bourse.php
+++ htdocs/bourse.php
@@ -6,6 +6,25 @@ require_once("header.php");
       <div class="row">
         <h1 style="text-align:center;">Bourse</h1>
       </div>
+        <ul class="nav nav-tabs">
+          <li class="active"><a data-toggle="tab" href="#bourse">Toute la Bourse</a></li>
+          <li><a data-toggle="tab" href="#recherche">Recherche</a></li>
+        </ul>
+
+      <div class="tab-content">
+
+          <div id="bourse" class="tab-pane fade in active">
+              <p>Liste de toute la bourse</p>
+          </div>
+
+
+           <div id="recherche" class="tab-pane fade">
+               <p>Le petit filtre des familles</p>
+
+          </div>
+
+      </div>
+   
     </div> <!-- /container -->
   </body>
 </html>
